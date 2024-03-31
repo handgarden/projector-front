47c0ebee439ccae9efdca587a4706490c23e3da4
handgarden <sjungwon03@gmail.com>
2024-03-31 22:13:45 +0900

    feat: axios 요청 함수 생성

---
+++ src/common/axios.ts
@@ -0,0 +1,57 @@
+import axios, { AxiosError } from "axios";
+import { ResponseStatus, RestResponse } from "../types/api/RestTemplate.type";
+
+export const axiosInstance = axios.create({
+  baseURL: process.env.REACT_APP_API_URL,
+  withCredentials: true,
+  timeout: 5000,
+});
+
+export const post = async <Req, Res>(
+  url: string,
+  body: Req,
+  token?: string
+): Promise<RestResponse<Res | null>> => {
+  try {
+    const response = await axiosInstance.post<RestResponse<Res>>(url, body, {
+      headers: {
+        Authorization: token ? `Bearer ${token}` : "",
+      },
+    });
+    return response.data;
+  } catch (e) {
+    const res = (e as AxiosError<RestResponse<null>>).response?.data;
+    if (!res) {
+      return {
+        status: ResponseStatus.SERVER_ERROR,
+        message: "서버 오류",
+        data: null,
+      };
+    }
+    return res;
+  }
+};
+
+export const get = async <Res>(
+  url: string,
+  token?: string
+): Promise<RestResponse<Res | null>> => {
+  try {
+    const response = await axiosInstance.get<RestResponse<Res>>(url, {
+      headers: {
+        Authorization: token ? `Bearer ${token}` : "",
+      },
+    });
+    return response.data;
+  } catch (e) {
+    const res = (e as AxiosError<RestResponse<null>>).response?.data;
+    if (!res) {
+      return {
+        status: ResponseStatus.SERVER_ERROR,
+        message: "서버 오류",
+        data: null,
+      };
+    }
+    return res;
+  }
+};
