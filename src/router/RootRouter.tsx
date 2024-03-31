8426792db317cbce98a0c056e809363091b2d1e5
handgarden <sjungwon03@gmail.com>
2024-03-31 22:20:54 +0900

    feat: 기본 라우터 추가

--- src/router/RootRouter.tsx
+++ src/router/RootRouter.tsx
@@ -1,10 +1,31 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import App from "../App";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import DefaultLayout from "../layout/DefaultLayout";
+import LoginPage from "../page/LoginPage";
+import { useAuthStore } from "../store/useAuthStore";
+import { useEffect } from "react";
+import { StateStatus } from "../types/common/StateStatus.type";
 export default function RootRouter() {
+  const isLogin = useAuthStore((state) => state.isLogin);
+  const status = useAuthStore((state) => state.status);
+  const tokenLogin = useAuthStore((state) => state.tokenLogin);
+
+  useEffect(() => {
+    tokenLogin();
+  }, [tokenLogin]);
+
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" Component={App} />
+        <Route path="/" Component={DefaultLayout}>
+          <Route
+            path="/login"
+            Component={
+              status !== StateStatus.INITIAL && isLogin
+                ? () => <Navigate to="/" />
+                : LoginPage
+            }
+          />
+        </Route>
       </Routes>
     </BrowserRouter>
   );
