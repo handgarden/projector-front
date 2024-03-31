b648ff5c842abe746b2ac761d7e5f9c569e53f6b
handgarden <sjungwon03@gmail.com>
2024-03-31 22:16:31 +0900

    feat: Navbar

---
+++ src/component/common/Navbar.tsx
@@ -0,0 +1,44 @@
+import LinkButton from "./LinkButton";
+import CenterCol from "./CenterCol";
+import CenterRow from "./CenterRow";
+import Title from "antd/es/typography/Title";
+import LogoutButton from "../auth/LogoutButton";
+
+type Props = {
+  isLogin: boolean;
+};
+
+export default function Navbar({ isLogin }: Props) {
+  return (
+    <CenterRow
+      style={{
+        height: "100%",
+        justifyContent: "space-between",
+        maxWidth: "1000px",
+        margin: "0 auto",
+      }}
+    >
+      <CenterCol style={{ height: "100%" }}>
+        <Title
+          level={1}
+          style={{
+            color: "white",
+            margin: 0,
+            fontSize: "1.5rem",
+            cursor: "pointer",
+          }}
+          onClick={() => (window.location.href = "/")}
+        >
+          Projector
+        </Title>
+      </CenterCol>
+      <CenterCol style={{ height: "100%" }}>
+        {isLogin ? (
+          <LogoutButton />
+        ) : (
+          <LinkButton to="/login">로그인</LinkButton>
+        )}
+      </CenterCol>
+    </CenterRow>
+  );
+}
