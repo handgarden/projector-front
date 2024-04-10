d0330f75e1205e09470e66e76509ae99de91a520
handgarden <sjungwon03@gmail.com>
2024-04-10 20:01:01 +0900

    feat: ApolloClient 설정

--- src/Provider.tsx
+++ src/Provider.tsx
@@ -1,7 +1,20 @@
+import { ApolloClient, ApolloProvider, InMemoryCache } from "@apollo/client";
+import useJwtToken from "./core/auth/hook/useJwtToken";
+
 type Props = {
   children: React.ReactNode;
 };
 
 export default function Provider({ children }: Props) {
-  return <>{children}</>;
+  const token = useJwtToken();
+  const client = new ApolloClient({
+    uri: process.env.REACT_APP_GRAPHQL_URL,
+    cache: new InMemoryCache(),
+    headers: {
+      authorization: `Bearer ${token}`,
+    },
+  });
+
+  return <ApolloProvider client={client}>{children}</ApolloProvider>;
 }
+
