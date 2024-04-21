3d76cd9342976cccb38bd77d2cc3024ace99b963
handgarden <sjungwon03@gmail.com>
2024-04-21 17:59:14 +0900

    fix: AuthButton에도 message 적용

--- app/auth/component/AuthButton.tsx
+++ app/auth/component/AuthButton.tsx
@@ -4,6 +4,7 @@ import { useAuthStore } from "../../../store/useAuthStore";
 import { useRouter } from "next/navigation";
 import usePathUtils from "../../../hook/usePathUtils";
 import { AUTH_PATH } from "../../../common/path/AuthPath";
+import { AUTH_MESSAGE_KR } from "../../../common/message/Auth.message";
 
 export function AuthButton() {
   const [isLogin, logout] = useAuthStore((state) => [
@@ -27,14 +28,14 @@ export function AuthButton() {
         }}
         variant="ghost"
       >
-        로그아웃
+        {AUTH_MESSAGE_KR.button.logout}
       </Button>
     );
   }
 
   return (
     <Button as={Link} href={loginRedirectPath} variant="ghost">
-      로그인
+      {AUTH_MESSAGE_KR.button.login}
     </Button>
   );
 }
