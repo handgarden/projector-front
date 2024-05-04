f0786fe6ad3de35031159f9620ce4aff8d0e8aa2
handgarden <sjungwon03@gmail.com>
2024-05-04 23:02:01 +0900

    fix: 페이지 높이 조절, navbar border bottom

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -2,7 +2,7 @@ import { ProjectList } from "./components/ProjectList";
 
 export default function ProjectsPage() {
   return (
-    <div className="min-h-full">
+    <div className="min-h-screen">
       <ProjectList />
     </div>
   );
