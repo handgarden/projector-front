9e92b265d3f4b378063caa87b6b979903d47dbcc
handgarden <sjungwon03@gmail.com>
2024-05-05 18:21:07 +0900

    fix: 프로젝트 아이템 열기 버튼 수정

--- app/projects/components/ProjectListItem.tsx
+++ app/projects/components/ProjectListItem.tsx
@@ -3,6 +3,7 @@ import { GetProjectsQuery } from "../../../gql/graphql";
 import { useState } from "react";
 import { FiExternalLink } from "react-icons/fi";
 import { DEFAULT_MESSAGE_KR } from "../../../common/message/Default.message";
+import { useRouter } from "next/navigation";
 type Props = {
   project: GetProjectsQuery["projects"]["items"][number];
 };
@@ -11,19 +12,19 @@ const ELLIPSIS_LENGTH = 300;
 
 export function ProjectListItem({ project }: Props) {
   const [ellipsisLength, setEllipsisLength] = useState(ELLIPSIS_LENGTH);
+  const router = useRouter();
 
   return (
     <Card className="my-4 mx-auto py-4 w-full max-w-[40rem]">
       <CardHeader className="pb-0 pt-2 px-4 flex items-center justify-between">
         <h3 className="text-large">{project.title}</h3>
-        <Button size="sm">
-          <a
-            href={`/projects/${project.id}`}
-            className="flex justify-center items-center"
-          >
-            {DEFAULT_MESSAGE_KR.navigate.open}
-            <FiExternalLink className="font-bold text-md ml-1 mb-[2px]" />
-          </a>
+        <Button
+          size="sm"
+          onClick={() => router.push(`/projects/${project.id}`)}
+          className="flex justify-center items-center"
+        >
+          {DEFAULT_MESSAGE_KR.navigate.open}
+          <FiExternalLink className="font-bold text-md mb-[2px]" />
         </Button>
       </CardHeader>
       <CardBody>
