0fa0fb15a7984f37910dc14249a4b8e59f69c98e
handgarden <sjungwon03@gmail.com>
2024-05-05 21:45:35 +0900

    feat: 슬라이드 리스트

--- common/components/button/DeleteItemButton.tsx
+++ common/components/button/DeleteItemButton.tsx
@@ -3,13 +3,14 @@ import { FiTrash } from "react-icons/fi";
 import { DEFAULT_MESSAGE_KR } from "../../message/Default.message";
 
 type Props = {
-  onClick: () => void;
+  onDelete: () => void;
   size?: "sm" | "md" | "lg";
+  className?: string;
 };
 
-export function DeleteItemButton({ onClick, size = "sm" }: Props) {
+export function DeleteItemButton({ onDelete, size = "sm", className }: Props) {
   return (
-    <Button size={size} onClick={onClick}>
+    <Button size={size} onClick={onDelete} className={className}>
       <FiTrash />
       {DEFAULT_MESSAGE_KR.button.delete}
     </Button>
